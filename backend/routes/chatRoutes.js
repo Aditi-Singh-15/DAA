49e6920c99ef0f74f76ceb95c38c618b621bbad9
Aditi Singh <aditiworks7@gmail.com>
2025-04-06 12:39:47 +0530

    Delete backend directory

--- backend/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateResponse } = require('../controllers/chatController');
-
-router.post('/chat', generateResponse);
-
-module.exports = router;
